be9da8b6a36ec90b01362dc0ea039411843ef228
Juan Pablo Ramirez L <jpramirezl@gmail.com>
2022-06-06 02:47:21 -0500

    secondPR

--- test__app.py
+++ test__app.py
@@ -5,4 +5,4 @@ def test_index_route():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data.decode('utf-8') == 'hello'
+    assert response.data.decode('utf-8') == 'hello1'
